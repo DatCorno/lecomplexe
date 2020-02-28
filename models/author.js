f286c490038828bbd16c75eddadb5331034c1673
François Corneau-Tremblay <corneau90@gmail.com>
2020-02-28 02:28:35 -0500

    Add author model

---
+++ models/author.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define('Author', {
+        firstname: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+        lastname: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+
+        picture: {
+            type: DataTypes.TEXT,
+        }
+    })
+};
